499aa9e1d1a50bf32cca5b0b9e9b3672bc7d485d
Ryu <thinkaboutcsharp@yahoo.co.jp>
2018-11-18 18:50:36 +0900

    Weight作成

--- Weight/Util/Matrix.cs
+++ Weight/Util/Matrix.cs
@@ -1,10 +1,83 @@
 ﻿using System;
-namespace Weight
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Weight.Util
 {
     public class Matrix
     {
-        public Matrix()
+        int rows;
+        int columns;
+        double[][] columnVectors;
+        double[][] transposedColumnVectors;
+
+        public Matrix(int rows, int columns)
         {
+            this.rows = rows;
+            this.columns = columns;
+
+            columnVectors = new double[columns][];
+            for (int c = 0; c < columns; c++)
+            {
+                columnVectors[c] = new double[rows];
+                for (int r = 0; r < rows; r++)
+                    columnVectors[c][r] = 0.0d;
+            }
+
+            transposedColumnVectors = new double[rows][];
+            for (int r = 0; r < rows; r++)
+            {
+                transposedColumnVectors[r] = new double[columns];
+                for (int c = 0; c < columns; c++)
+                    transposedColumnVectors[r][c] = 0.0d;
+            }
+        }
+
+        public void SetValue(int row, int column, double value)
+        {
+            columnVectors[column][row] = value;
+            transposedColumnVectors[row][column] = value;
+        }
+
+        public double GetValue(int row, int column)
+        {
+            return columnVectors[column][row];
+        }
+
+        public double[] Dot(double[] vector)
+        {
+            var result = new double[columns];
+
+            for (int c = 0; c < columns; c++)
+            {
+                var columnVector = columnVectors[c];
+                double product = 0.0d;
+                for (int e = 0; e < rows; e++)
+                {
+                    product += vector[e] * columnVector[e];
+                }
+                result[c] = product;
+            }
+
+            return result;
+        }
+
+        public double[] DotTransposed(double[] vector)
+        {
+            var result = new double[rows];
+
+            for (int r = 0; r < rows; r++)
+            {
+                var columnVector = transposedColumnVectors[r];
+                double product = 0.0d;
+                for (int e = 0; e < columns; e++)
+                {
+                    product += vector[e] * columnVector[e];
+                }
+                result[r] = product;
+            }
+
+            return result;
         }
     }
 }
--- Weight/WeightMatrix.cs
+++ Weight/WeightMatrix.cs
@@ -3,8 +3,32 @@ namespace Weight
 {
     public class WeightMatrix
     {
-        public WeightMatrix()
+        public int InputSize { get; }
+        public int OutputSize { get; }
+
+        Util.Matrix weights;
+
+        public WeightMatrix(int input, int output)
         {
+            weights = new Util.Matrix(input, output);
+            InputSize = input;
+            OutputSize = output;
+        }
+
+        public double this[int row, int column]
+        {
+            get => weights.GetValue(row, column);
+            set => weights.SetValue(row, column, value);
+        }
+
+        public double[] Forward(double[] input)
+        {
+            return weights.Dot(input);
+        }
+
+        public double[] Back(double[] output)
+        {
+            return weights.DotTransposed(output);
         }
     }
 }
